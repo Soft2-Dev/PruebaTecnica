f00dface4e52fca9507a3f4466ccc47da38d3515
zackill5 <87101458+zackill5@users.noreply.github.com>
2022-10-24 15:09:10 -0500

    actualizacion3

--- double_v_partners/lib/main.dart
+++ double_v_partners/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pelis',
+      title: 'Registros',
       home: const SplashScreenPage(),
       initialRoute: 'splas',
       routes: getRutas(),
